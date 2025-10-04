7fbe4f3ef12e7642646d74b896d24695baa5d484
David Osemwegie <35180584+davidosemwegie@users.noreply.github.com>
2025-10-04 11:38:42 -0400

    done with setting up form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
 "use client";
 
-export default function Home() {
-  const handleClick = () => {
-    console.log("Button clicked");
-  };
+import { SetUserForm } from "@/components/set-user-form/set-user-form";
 
+export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
-      <button onClick={handleClick}>Click me</button>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <SetUserForm />
     </div>
   );
 }
---
+++ src/components/set-user-form/set-user-form.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "../ui/button";
+import {
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+  Form,
+} from "../ui/form";
+import { Input } from "../ui/input";
+
+const formSchema = z.object({
+  firstName: z.string().min(1),
+  lastName: z.string().min(1),
+});
+
+export function SetUserForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="firstName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="shadcn"
+                  {...field}
+                  onChange={(e) => field.onChange(e.target.value)}
+                  data-intent="settings.update-name.fields.firstName"
+                />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="lastName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Last Name</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="Doe"
+                  {...field}
+                  onChange={(e) => field.onChange(e.target.value)}
+                  data-intent="settings.update-name.fields.lastName"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button data-intent="settings.update-name.actions.update" type="submit">
+          Submit
+        </Button>
+      </form>
+    </Form>
+  );
+}
